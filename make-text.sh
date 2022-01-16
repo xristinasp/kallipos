4a41c26b6f8c0cbcbafe572e3b33fb4eb5796a4d
epidrome <github@epidro.me>
2022-01-16 18:23:34 +0200

    updated filenames

--- make-text.sh
+++ make-text.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-for filename in text/mi-*.md; do
+for filename in text/ch*.txt; do
    [ -e "$filename" ] || continue
-   pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --citeproc --bibliography=bibliography/"$(basename "$filename" .md).bib" --to latex > latex/"$(basename "$filename" .md).tex"
+   pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --to latex > latex/"$(basename "$filename" .txt).tex"
 done
