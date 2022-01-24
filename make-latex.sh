97721a2cff4b795bf725d2d71d44d2fca213e4b5
epidrome <github@epidro.me>
2022-01-24 17:38:36 +0200

    latex workflow

---
+++ make-latex.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#assemble and preprocess all the sources files
+
+pandoc text/pre.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/pre.tex
+pandoc text/intro.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/intro.tex
+
+for filename in text/ch*.txt; do
+   [ -e "$filename" ] || continue
+   pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --to latex > latex/"$(basename "$filename" .txt).tex"
+done
+
+pandoc text/epi.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/epi.tex
+
--- make-text.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for filename in text/ch*.txt; do
-   [ -e "$filename" ] || continue
-   pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --to latex > latex/"$(basename "$filename" .txt).tex"
-done
