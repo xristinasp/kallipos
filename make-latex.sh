5facba6467388c209b595450e52b7c1921954e4e
epidrome <github@epidro.me>
2022-02-03 19:24:32 +0200

    greek figure labels

--- make-latex.sh
+++ make-latex.sh
@@ -9,5 +9,14 @@ for filename in text/ch*.txt; do
    pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --to latex > latex/"$(basename "$filename" .txt).tex"
 done
 
+sed -i '' 's+hyperlink{fig+hyperlink{Εικόνα fig+g' ./latex/ch0*
+sed -i '' 's+caption{Figure+caption{Εικόνα+g' ./latex/ch0*
+
 pandoc text/epi.txt --lua-filter=epigraph.lua --to markdown | pandoc --top-level-division=chapter --to latex > latex/epi.tex
+
+for filename in text/apx*.txt; do
+   [ -e "$filename" ] || continue
+   pandoc --lua-filter=extras.lua "$filename" --to markdown | pandoc --lua-filter=extras.lua --to markdown | pandoc --lua-filter=epigraph.lua --to markdown | pandoc --lua-filter=figure.lua --to markdown | pandoc --filter pandoc-fignos --to markdown | pandoc --metadata-file=meta.yml --top-level-division=chapter --citeproc --bibliography=bibliography/"$(basename "$filename" .txt).bib" --reference-location=section --to latex > latex/"$(basename "$filename" .txt).tex"
+done
+
 
