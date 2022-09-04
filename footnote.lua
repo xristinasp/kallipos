990b2fb05cfcfb0f2cd25cf1a3db1ed54ae9a40d
epidrome <github@epidro.me>
2022-09-04 10:35:58 +0300

    added lua filter for license_text content

--- footnote.lua
+++ footnote.lua
@@ -5,11 +5,13 @@ function Note(el)
     local ft = stringify(el.content[1]) 
     if string.sub(ft,1,4) == "fig:" then
         local fn = string.sub(ft,5)
+        --print(fn)
         local f = io.open("figures/" .. fn .. ".md", 'r')
         local doc = pandoc.read(f:read('*a'))
         f:close()
         local title=pandoc.utils.stringify(doc.meta.title) or "Title has not been set"
-        return pandoc.Note("*@" .. ft .. " " .. title)
+        local license_text=pandoc.utils.stringify(doc.meta.license_text) or "License has not been set"
+        return pandoc.Note("*@" .. ft .. " " .. title .. " (" .. license_text .. ")")
     end
   end
 end
